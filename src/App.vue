6819c902b5c50e021a83fc2b991649254d814bb2
Niels Bos <nielsbos97@gmail.com>
2022-04-07 17:37:09 +0200

    commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+      <v-row>
+        <v-col cols="3">
+          <v-range-slider></v-range-slider>
+        </v-col>
+      </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data: () => ({
+    //
+  }),
+}
+</script>
